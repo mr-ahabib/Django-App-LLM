fb0c9504e4dda8cc20a82ad96a417959a3b08e08
mr-ahabib <mr.ahabib261@gmail.com>
2024-07-18 00:28:22 +0600

    static

--- hello/urls.py
+++ hello/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('std_assignment.urls')),
-    path('',include('std_assignment.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
-+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('',include('std_assignment.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
