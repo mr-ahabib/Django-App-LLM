5f846d51329b604d91cdf68ff83aedaa4154c651
mr-ahabib <mr.ahabib261@gmail.com>
2024-07-18 00:00:49 +0600

    with static

--- hello/urls.py
+++ hello/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('std_assignment.urls')),
     path('',include('std_assignment.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
++static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
